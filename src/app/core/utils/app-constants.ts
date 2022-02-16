ae68f9f41dc93c38f0f28c5bbefd4974cd3bff31
Odofin Oyejide <odofin@swipe.ng>
2022-02-16 18:25:48 +0100

    Created application constant app-constants.ts  with various constants variables and updated environment.ts with base api location

---
+++ src/app/core/utils/app-constants.ts
@@ -0,0 +1,10 @@
+export class AppConstant{
+    public static SUCCESS='Success';
+    public static FAILED='Fail';
+    public static PAGE:number=0;
+    public static SIZE:number=50
+    public static MESSAGE='message';
+    public static DATA='data';
+
+
+}
