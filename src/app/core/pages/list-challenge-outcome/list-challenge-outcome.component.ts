879eca66e40701a672b513b76feb50d1021b95b6
Odofin Oyejide <odofin@swipe.ng>
2022-02-16 19:05:15 +0100

    consume list of top three candidate with most successful submission

--- src/app/core/pages/list-challenge-outcome/list-challenge-outcome.component.ts
+++ src/app/core/pages/list-challenge-outcome/list-challenge-outcome.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskReportResponse } from '../../model/app-model';
+import { AppService } from '../../services/app.service';
+import { AppConstant } from '../../utils/app-constants';
 
 @Component({
   selector: 'app-list-challenge-outcome',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-challenge-outcome.component.scss']
 })
 export class ListChallengeOutcomeComponent implements OnInit {
-
-  constructor() { }
+  dataList:TaskReportResponse[]
+  errorMessage:any
+  loading:boolean;
+  constructor(private appService:AppService) { }
 
   ngOnInit(): void {
+    this.listTopThreeSuccessfulSolution()
   }
+
+  listTopThreeSuccessfulSolution(){
+    this.loading = true
+    this.appService.listTopThreeSuccess().subscribe(
+      (result: any) => {
+        if (result[AppConstant.MESSAGE] == AppConstant.SUCCESS) {
+          this.dataList = result[AppConstant.DATA];
+        } else {
+          this.errorMessage = result[AppConstant.DATA];
+          console.log(result[AppConstant.DATA])
+        }
+      },
+      (error) => {
+        this.appService.toastService.error(error, AppConstant.FAILED);
+      }
+    );
+  }
+}
 
 }
