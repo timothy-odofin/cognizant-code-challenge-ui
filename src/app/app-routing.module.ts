cd5e763b8fa6334dad44ae345681815b7bca69cb
Odofin Oyejide <odofin@swipe.ng>
2022-02-16 19:09:09 +0100

    created application routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ListChallengeOutcomeComponent } from './core/pages/list-challenge-outcome/list-challenge-outcome.component';
+import { SolveChallengeComponent } from './core/pages/solve-challenge/solve-challenge.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'challenge', pathMatch: 'full' },
+  {path: "challenge", component: SolveChallengeComponent},
+  {path: "top-three-candidate", component: ListChallengeOutcomeComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
