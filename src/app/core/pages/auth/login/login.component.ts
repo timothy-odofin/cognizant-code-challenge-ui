e10bac5ca8d011180b19448c70ae6418584e7277
Odofin Oyejide <odofin@swipe.ng>
2022-02-22 06:34:26 +0100

    Added loginForm Group

--- src/app/core/pages/auth/login/login.component.ts
+++ src/app/core/pages/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  loginForm: FormGroup = new FormGroup({});
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl ('', [Validators.required]),
+      password: new FormControl ('', [Validators.required]),
+    })
+  }
+ get email() {
+    return this.loginForm.get('email');
+  }
+
+   get password() {
+    return this.loginForm.get('password');
   }
 
 }
