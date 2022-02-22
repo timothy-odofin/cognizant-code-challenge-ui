a082f51827db3620c8ebe819bdff4a99e7e728ed
Odofin Oyejide <odofin@swipe.ng>
2022-02-22 09:07:57 +0100

    Formatted AppService

--- src/app/core/services/app.service.ts
+++ src/app/core/services/app.service.ts
@@ -8,40 +8,45 @@ import { CompileUiPayload, Login, Signup } from '../model/app-model';
 import { AppConstant } from '../utils/app-constants';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppService {
   base_api = environment.base_api;
   loginUser: any;
 
-  private isLoggedin: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private isLoggedin: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
 
-  constructor(private http: HttpClient, public toastService:ToastrService, private router:Router) { 
-    this.checkLoginStatus()
+  constructor(
+    private http: HttpClient,
+    public toastService: ToastrService,
+    private router: Router
+  ) {
+    this.checkLoginStatus();
   }
   loginStatus(): Observable<boolean> {
     return this.isLoggedin.asObservable();
   }
-  getToken():string{
-return localStorage.getItem(AppConstant.USER_TOKEN)!
+  getToken(): string {
+    return localStorage.getItem(AppConstant.USER_TOKEN)!;
   }
-saveUserToStore(key:string,payload:any){
-  localStorage.setItem(key,JSON.stringify(payload))
-}
+  saveUserToStore(key: string, payload: any) {
+    localStorage.setItem(key, JSON.stringify(payload));
+  }
   checkLoginStatus() {
-    let login:any =  JSON.parse(localStorage.getItem(AppConstant.LOGIN_USER)!)
-    
+    let login: any = JSON.parse(localStorage.getItem(AppConstant.LOGIN_USER)!);
+
     if (login !== null) {
-      this.loginUser = login
+      this.loginUser = login;
       this.isLoggedin.next(true);
       // this.idleLogout()
-    }
-    else {
+    } else {
       this.isLoggedin.next(false);
     }
   }
 
-  setLoginStatus(val:boolean): void {
+  setLoginStatus(val: boolean): void {
     this.isLoggedin.next(val);
   }
 
@@ -50,52 +55,40 @@ saveUserToStore(key:string,payload:any){
     this.isLoggedin.next(false);
     this.router.navigateByUrl('/');
   }
-  fetchTasks(page:number,size:number) {
+  fetchTasks(page: number, size: number) {
     return this.http
-      .get(
-        this.base_api +
-          `api/task/list?page=${page}&size=${size}`
-      )
+      .get(this.base_api + `api/task/list?page=${page}&size=${size}`)
       .pipe(catchError(async (err) => this.errorHandler(err)));
   }
   fetchSupportedLanguages() {
     return this.http
-      .get(
-        this.base_api +
-          `api/script/languages`
-      )
+      .get(this.base_api + `api/script/languages`)
       .pipe(catchError(async (err) => this.errorHandler(err)));
   }
   listTopThreeSuccess() {
     return this.http
-      .get(
-        this.base_api +
-          `api/task/report`
-      )
+      .get(this.base_api + `api/task/report`)
       .pipe(catchError(async (err) => this.errorHandler(err)));
   }
 
-  sendCodeChallenge(payload:CompileUiPayload) {
+  sendCodeChallenge(payload: CompileUiPayload) {
     return this.http
       .post(this.base_api + `api/script/execute`, payload)
       .pipe(catchError(async (err) => this.errorHandler(err)));
   }
-  
 
-  signup(payload:Signup) {
+  signup(payload: Signup) {
     return this.http
       .post(this.base_api + `entrance/create-account`, payload)
       .pipe(catchError(async (err) => this.errorHandler(err)));
   }
-  
 
-  login(payload:Login) {
+  login(payload: Login) {
     return this.http
       .post(this.base_api + `entrance/signin`, payload)
       .pipe(catchError(async (err) => this.errorHandler(err)));
   }
   errorHandler(error: HttpErrorResponse) {
-    this.toastService.error(error.message,'Server Error')
-    
+    this.toastService.error(error.message, 'Server Error');
   }
 }
