48bdda310178071f5c5de71163b7cd56d7c6c2da
Odofin Oyejide <odofin@swipe.ng>
2022-02-22 08:06:36 +0100

    Created signup functionalities

--- src/app/core/pages/auth/signup/signup.component.ts
+++ src/app/core/pages/auth/signup/signup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Signup } from 'src/app/core/model/app-model';
+import { AppService } from 'src/app/core/services/app.service';
+import { AppConstant } from 'src/app/core/utils/app-constants';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +11,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  signupForm: FormGroup= new FormGroup({});
+  submitLoading:boolean=false;
+  errorMessage:any
+  constructor(private appService:AppService, private route:Router) { }
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      name: new FormControl ('', [Validators.required]),
+      userName: new FormControl ('', [Validators.required]),
+      password: new FormControl ('', [Validators.required]),
+      comfirmPassword: new FormControl ('', [Validators.required]),
+    })
   }
+  closeMessage() {
+    setTimeout(() => {
+      this.errorMessage = null
+    }, 5000);
+  }
+  navigateToLogin() {
+    setTimeout(() => {
+      this.errorMessage = null
+      this.route.navigateByUrl("/")
+    }, 5000);
+  }
+  signup() {
+    if(this.signupForm.invalid){
+      return
+    }
+    this.submitLoading = true
+    this.errorMessage = undefined;
+    let payload: Signup = new Signup();
+    payload.username = this.userName?.value;
+    payload.password = this.password?.value;
+    payload.fullname = this.name?.value;
+    this.appService.signup(payload).subscribe((result: any) => {
+      if (result[AppConstant.MESSAGE] == AppConstant.SUCCESS) {
+        this.submitLoading = false
+        this.appService.toastService.success(result[AppConstant.DATA],result[AppConstant.MESSAGE])
+        this.navigateToLogin()
+      } else {
+        this.submitLoading = false
+        this.errorMessage = result[AppConstant.DATA];
+        this.closeMessage();
+      }
+    });
+    this.signupForm.reset()
+  }
+
+   get name() {
+    return this.signupForm.get('name');
+  }
+
+   get userName() {
+    return this.signupForm.get('userName');
+  }
+
+   get password() {
+    return this.signupForm.get('password');
+  }
+
+  get comfirmPassword() {
+    return this.signupForm.get('comfirmPassword');
+  }
+
 
 }
