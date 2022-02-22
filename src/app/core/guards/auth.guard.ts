77698f3607ddedffb5c5ccf3cbe7c5589aa688a5
Odofin Oyejide <odofin@swipe.ng>
2022-02-22 09:25:34 +0100

    Implemented Authguard functionalities

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AppService } from '../services/app.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
+  LoginStatus: boolean;
   
+  constructor(private appService: AppService, private router: Router) {
+    this.ListentoLoginStatus();
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {    
+      if (this.LoginStatus) {
+        return true;
+      }
+
+      else{
+        this.router.navigateByUrl('/');
+        return false;
+      }
+  }
+
+  ListentoLoginStatus() {
+    this.appService.loginStatus().subscribe(res => {
+      this.LoginStatus = res; 
+    });
+  }
 }
